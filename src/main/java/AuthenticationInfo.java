46c2d76c67946e9a8c967a5dc7124c8ca73eb97d
carllindiii <carl.lind.iii@gmail.com>
2015-10-15 15:55:50 -0700

    comitting main

---
+++ src/main/java/AuthenticationInfo.java
@@ -0,0 +1,11 @@
+/**
+ * This is a default file containing each of our auth info. It is on github, but is in the gitignore so we don't
+ * commit this file with our super secret information.
+ */
+public class AuthenticationInfo {
+    String applicationClientID = "Your application client ID";
+    String clientSecret = "Your client secret here";
+    String code = "Your code here";
+
+    public
+}
