c8e24c41dff1661f79e12bab76a113af5d402d50
zarend <zacharyarend@gmail.com>
2015-12-03 11:04:37 -0800

    segments_avgTimePerDistance: move stuff out of main

--- metrics/time/segments_avgTimePerDistance.go
+++ metrics/time/segments_avgTimePerDistance.go
@@ -12,12 +12,69 @@ import (
 	"github.com/strava/go.strava"
 )
 
+func doTheThing(segment *strava.SegmentDetailed, service *strava.SegmentsService) {
+	const KM_PER_MILE = 1.60934
+	const SEC_TO_MIN = 60
+
+	segmentId := segment.Id
+
+	verb := "ridden"
+	if segment.ActivityType == strava.ActivityTypes.Run {
+		verb = "run"
+	}
+	fmt.Printf("%s, %s %d times by %d athletes\n",
+			 segment.Name, verb, segment.EffortCount, segment.AthleteCount)
+
+	//for all the segments, grab and calculate all the segment efforts
+	var totalMovingTimeInSeconds int
+	var totalElapsedTimeInSeconds int
+	// return list of segment efforts: this is PAGED
+	segmentEfforts, err := service.ListEfforts(segmentId).Do()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	segmentEffortsSliceLen := len(segmentEfforts)
+	fmt.Printf("Averages over first %d results\n", segmentEffortsSliceLen)
+
+	for _, segmentEffortSummary := range segmentEfforts {
+		if(segmentEffortSummary.MovingTime > 0) {
+				totalMovingTimeInSeconds += segmentEffortSummary.MovingTime;
+			}
+			if(segmentEffortSummary.ElapsedTime > 0) {
+				totalElapsedTimeInSeconds += segmentEffortSummary.ElapsedTime;
+			}
+	}   
+
+	// moving time
+	avgMovingPace := float64(totalMovingTimeInSeconds) / float64(segmentEffortsSliceLen)
+	avgMovingPace /= SEC_TO_MIN
+	// calculating distances
+	kmDistance := segment.Distance / 1000
+	avgMovingPacePerKm := avgMovingPace / kmDistance
+	avgMovingPacePerMile := avgMovingPacePerKm * KM_PER_MILE
+	// summary text
+	fmt.Printf("MOVING: Total distance of %.2f km %s with an average pace of %.2f min per km (%.2f min per mile)\n",
+		kmDistance, verb, avgMovingPacePerKm, avgMovingPacePerMile)
+
+	// elapsed time
+	avgElapsedPace := float64(totalElapsedTimeInSeconds) / float64(segmentEffortsSliceLen)
+	avgElapsedPace /= SEC_TO_MIN
+	// calculating distances
+	kmDistance = segment.Distance / 1000
+	avgElapsedPacePerKm := avgElapsedPace / kmDistance
+	avgElapsedPacePerMile := avgElapsedPacePerKm * KM_PER_MILE
+	// summary text
+	fmt.Printf("ELAPSED: Total distance of %.2f km %s with an average pace of %.2f min per km (%.2f min per mile)\n\n",
+		kmDistance, verb, avgElapsedPacePerKm, avgElapsedPacePerMile)
+}
+
 func main() {
 	var segmentId int64
 	var accessToken string
 	const SEGMENT_SIZE = 10
-	const KM_PER_MILE = 1.60934
-	const SEC_TO_MIN = 60
 
 	segmentList := [SEGMENT_SIZE]int64{365235, 6452581, 664647, 1089563, 4956199,
 					 		2187, 5732938, 654030, 616554, 3139189};
@@ -48,50 +105,6 @@ func main() {
 			os.Exit(1)
 		}
 
-		verb := "ridden"
-		if segment.ActivityType == strava.ActivityTypes.Run {
-			verb = "run"
-		}
-		fmt.Printf("%s, %s %d times by %d athletes\n",
-				 segment.Name, verb, segment.EffortCount, segment.AthleteCount)
-
-		//for all the segments, grab and calculate all the segment efforts
-		var totalMovingTimeInSeconds int
-		var totalElapsedTimeInSeconds int
-		// return list of segment efforts: this is PAGED
-		segmentEfforts, err := service.ListEfforts(segmentId).Do()
-		segmentEffortsSliceLen := len(segmentEfforts)
-		fmt.Printf("Averages over first %d results\n", segmentEffortsSliceLen)
-
-		for _, segmentEffortSummary := range segmentEfforts {
-			if(segmentEffortSummary.MovingTime > 0) {
-   				totalMovingTimeInSeconds += segmentEffortSummary.MovingTime;
-   			}
-   			if(segmentEffortSummary.ElapsedTime > 0) {
-   				totalElapsedTimeInSeconds += segmentEffortSummary.ElapsedTime;
-   			}
-		}   
-
-		// moving time
-		avgMovingPace := float64(totalMovingTimeInSeconds) / float64(segmentEffortsSliceLen)
-		avgMovingPace /= SEC_TO_MIN
-		// calculating distances
-		kmDistance := segment.Distance / 1000
-		avgMovingPacePerKm := avgMovingPace / kmDistance
-		avgMovingPacePerMile := avgMovingPacePerKm * KM_PER_MILE
-		// summary text
-		fmt.Printf("MOVING: Total distance of %.2f km %s with an average pace of %.2f min per km (%.2f min per mile)\n",
-			kmDistance, verb, avgMovingPacePerKm, avgMovingPacePerMile)
-
-		// elapsed time
-		avgElapsedPace := float64(totalElapsedTimeInSeconds) / float64(segmentEffortsSliceLen)
-		avgElapsedPace /= SEC_TO_MIN
-		// calculating distances
-		kmDistance = segment.Distance / 1000
-		avgElapsedPacePerKm := avgElapsedPace / kmDistance
-		avgElapsedPacePerMile := avgElapsedPacePerKm * KM_PER_MILE
-		// summary text
-		fmt.Printf("ELAPSED: Total distance of %.2f km %s with an average pace of %.2f min per km (%.2f min per mile)\n\n",
-			kmDistance, verb, avgElapsedPacePerKm, avgElapsedPacePerMile)
+		doTheThing(segment, service)
 	}
 }
