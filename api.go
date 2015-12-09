30168c3d6b1022d68aa0bf65174e51de62d9f322
zarend <zacharyarend@gmail.com>
2015-12-08 16:51:56 -0800

    directory structure for api

---
+++ api.go
@@ -0,0 +1,32 @@
+package stravaAI
+
+import (
+   "github.com/strava/go.strava"
+
+   "./metrics/path"
+   "./metrics/time"
+   "./metrics/elevation"
+)
+
+type Client struct {
+   stravaClient *strava.Client
+   service *strava.SegmentsService
+}
+
+type DIFFICULTY_LABEL int
+
+const (
+   EASY DIFFICULTY_LABEL = iota
+   MEDIUM = iota
+   HARD = iota
+)
+
+func NewClient(stravaClient strava.Client) *Client {
+   var stravaAI Client
+   stravaAI.stravaClient = stravaClient
+}
+
+func (client *Client) getSegmentDifficulty(segment *strava.SegmentDetailed) DIFFICULTY_LABEL {
+   // TODO
+   return EASY
+}
